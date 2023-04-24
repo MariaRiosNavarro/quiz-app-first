56a7f7f36385fc6eab5c86389927f019ade91707
Maria Rios Navarro <propscode254@gmail.com>
2023-04-24 21:24:37 +0200

    add the script link

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+function showAnswer() {
+  const answerElement = document.getElementById("answer-one");
+  const answerButton = document.querySelector(".answer");
 
-  function showAnswer() {
-    const answerElement = document.getElementById("answer-one"); 
-    const answerButton = document.querySelector(".answer");
-  
-    answerElement.innerHTML = "Hypertext Markup Language";
+  answerElement.innerHTML = "Hypertext Markup Language";
+  answerButton.classList.toggle("hide-answer");
+
+  answerButton.onclick = function () {
+    answerElement.innerHTML = "";
     answerButton.classList.toggle("hide-answer");
-  
-    answerButton.onclick = function () {
-      answerElement.innerHTML = "";
-      answerButton.classList.toggle("hide-answer");
-    };
-  }
+  };
+}
