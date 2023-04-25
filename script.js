2dc2d4368f8819728c86cd17e11f4a5ef247c544
Maria Rios Navarro <propscode254@gmail.com>
2023-04-25 12:52:33 +0200

    Test

--- script.js
+++ script.js
@@ -12,11 +12,11 @@
 // };
 // FUNKTIONIERT NICHT
 
-// const body = document.querySelector('[data-js="js-body"]');
-// const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
+const body = document.querySelector('[data-js="js-body"]');
+const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
 
-// toggleDarkmode.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-// });
+toggleDarkmode.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
 
 // funktioniert nicht
