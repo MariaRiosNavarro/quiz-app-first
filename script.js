960fa5e9e46ebab0b2d9911c4007adc073e536bd
Maria Rios Navarro <propscode254@gmail.com>
2023-04-25 12:58:46 +0200

    dark Mode works in user page but not in the other

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// function showAnswer() {
-//   const answerParagraph = document.getElementById("answer-one");
-//   const answerButton = document.querySelector(".answer-one-button");
-
-//   answerParagraph.innerHTML = "Hypertext Markup Language";
-//   console.log(answerElement);
-// }
-// FUNKTIONIERT NICHT
-
-// document.getElementById("btn").onclick = function () {
-//   console.log("Hypertext Markup Language");
-// };
-// FUNKTIONIERT NICHT
 
 const body = document.querySelector('[data-js="js-body"]');
 const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
@@ -19,4 +6,3 @@ toggleDarkmode.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
 
-// funktioniert nicht
