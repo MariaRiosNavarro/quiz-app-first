7e7e05c13677f947c83f4f594a556ff5dfd26390
Maria Rios Navarro <propscode254@gmail.com>
2023-04-26 11:45:19 +0200

    Test with the class instead of attribut. dont work

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-// glogal js
+// -------------glogal js-----------------------------
 
 const body = document.querySelector('[data-js="js-body"]');
 
-// // user page: darkmode switch
+// ------------user page: darkmode switch--------------
 
-// const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
+const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
 
-// toggleDarkmode.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-// });
+toggleDarkmode.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
 
-//to  check:
-// icon user in weis machen
-// event übertragber für alle seiten.
-// title in schwarz lassen
+// Works but todo:
 
-// Question-answer tests
+// icon user white
+// event for all the pages
+// header in black
 
-// Warning speziall case  to JS-another structure!!! I have a pre inside the p!! in question 3 and 4 -->
+//--------------Question-answer js--------------------------
+
+// Warning speziall in question 3 and 4 _ JS-another structure!!! I have a pre inside the p!! in question 3 and 4 -->
+
+// ???????WHY THAT DOESNT WORK?????????
 
 const answerOne = document.querySelector('[data-js="answer-one"]');
 const answerButton = document.querySelector('[data-js="answer-button-one"]');
 
 answerButton.addEventListener("click", () => {
-  answerOne.classList.toggle("hidden");
+  answerOne.classList.toggle("question-card__answer--hidden");
 });
 
-
-// why doesnt work???
+// Allready done: check the script @ header (everything ok, dark mode works )
