f073e190d0f0943599a68ec89d3d1ed08bed6435
Maria Rios Navarro <propscode254@gmail.com>
2023-04-26 15:41:49 +0200

    add Felines Changes

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 // -------------glogal js-----------------------------
-
+​
 // const body = document.querySelector('[data-js="js-body"]');
-
+​
 // // ------------user page: darkmode switch--------------
-
-// const darkModeToggle = document.querySelector('[data-js="switch-dark"]');
-
-// darkModeToggle.addEventListener("click", () => {
+​
+// const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
+​
+// toggleDarkmode.addEventListener("click", () => {
 //   body.classList.toggle("dark");
 // });
-
-// Works but todo:
-
-// icon user white
-// event for all the pages
-// header in black
-
-//--------------Question-answer js--------------------------
-
-// Warning speziall in question 3 and 4 _ JS-another structure!!! I have a pre inside the p!! in question 3 and 4 -->
-
-// ???????WHY THAT DOESNT WORK?????????
-
-const answerOne = document.querySelector('[data-js="answer-one"]');
+​
+// // Works but todo:
+​
+// // icon user white
+// // event for all the pages
+// // header in black
+​
+// //--------------Question-answer js--------------------------
+​
 const answerButtonOne = document.querySelector('[data-js="answer-button-one"]');
-const testButton = document.querySelector('[data-js="test-button"]');
-
-// answerButtonOne.addEventListener("click", () => {
-//   // answerOne.classList.toggle("question-card__answer--hidden");
-//   console.log("test");
-// });
-
-// // Allready done: check the script @ header (everything ok, dark mode works )
-
-testButton.addEventListener("click", () => {
-  console.log("test");
+const answerButtonTwo = document.querySelector('[data-js="answer-button-two"]');
+const answerButtonThree = document.querySelector(
+  '[data-js="answer-button-three"]'
+);
+const firstAnswer = document.querySelector('[data-js="answer-one"]');
+const secondAnswer = document.querySelector('[data-js="answer-two"]');
+const thirdAnswer = document.querySelector('[data-js="answer-three"]');
+​
+answerButtonOne.addEventListener("click", () => {
+  console.log("show answer");
+  firstAnswer.classList.toggle("question-card__answer--hidden");
+});
+​
+answerButtonTwo.addEventListener("click", () => {
+  console.log("show answer");
+  secondAnswer.classList.toggle("question-card__answer--hidden");
+});
+​
+answerButtonThree.addEventListener("click", () => {
+  console.log("show answer");
+  thirdAnswer.classList.toggle("question-card__answer--hidden");
 });
