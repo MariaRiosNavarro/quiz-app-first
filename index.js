1db3629d004a89dafa153eb40eae28fa3129d746
Maria Rios Navarro <propscode254@gmail.com>
2023-04-26 20:08:09 +0200

    separate js files and now work everything

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// -------------glogal js-----------------------------
-// const body = document.querySelector('[data-js="js-body"]');
-
-// ------------user page: darkmode switch--------------
-
-// const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
-
-// toggleDarkmode.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-// });
-
-// // Works but todo:
-
-// // icon user white
-// // event for all the pages
-// // header in black
 
 // //--------------Question-answer js--------------------------
 
---
+++ user.js
@@ -0,0 +1,16 @@
+// -------------glogal js-----------------------------
+const body = document.querySelector('[data-js="js-body"]');
+
+// ------------user page: darkmode switch--------------
+
+const toggleDarkmode = document.querySelector('[data-js="switch-dark"]');
+
+toggleDarkmode.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+
+// // Works but todo:
+
+// // icon user white
+// // event for all the pages
+// // header in black
